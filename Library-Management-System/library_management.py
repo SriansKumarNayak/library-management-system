8c004dbcedef0987a599bd93c970662161fc3d11
Srians Kumar Nayak <sriansnayak07@gmail.com>
2026-03-28 23:28:51 +0530

    Add JSON file saving to library system
    Added JSON file handling to save and load books data. Improved library system with persistent storage.

--- Library-Management-System/library_management.py
+++ Library-Management-System/library_management.py
@@ -1,3 +1,4 @@
+import json
 class Book:
 
     book_id_counter = 101
@@ -12,7 +13,40 @@ class Book:
     def display(self):
         print(f"Book ID: {self.book_id}, Title: {self.title}, Author: {self.author}, Status: {self.status}")
 
+def save_books():
+    data = []
+
+    for book in books:
+        data.append({
+            "book_id" : book.book_id,
+            "title" : book.title,
+            "author" : book.author,
+            "status" : book.status
+        })
+        
+    with open("books.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+def load_books():
+    try:
+        with open("books.json", "r") as file:
+            data = json.load(file)
+
+            for item in data:
+                b = Book(item(["title"], item["author"]))
+                b.book_id = item["book_id"]
+                b.status = item["status"]
+                books.append(b)
+
+            if books:
+                Book.book_id_counter = books[-1].book_id + 1
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        pass
+
 books = []
+load_books()
+
 while True:
     print("\n===== Library Management System =====\n")
     print("1. Add Book")
@@ -45,6 +79,7 @@ while True:
                 print("\nAuthor cannot be empty...\n")
 
         books.append(Book(title, author))
+        save_books()
         print("----------------------------------")
         print("Book added successfully!")
         print(f"Book ID: {books[-1].book_id}")
@@ -102,6 +137,7 @@ while True:
                             print("Book is already Issued")
                         else:
                             book.status = "Issued"
+                            save_books()
                             print(f"\nBook Issued Successfully! (ID: {book.book_id})\n")
                         found = True
                         break
@@ -130,6 +166,7 @@ while True:
                         print("\nBook is already available.\n")
                     else:
                         book.status = "Available"
+                        save_books()
                         print("\nBook Returned Successfully!")
                     found = True
                     break
